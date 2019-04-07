2a025692a2146610d3f3aca2676a1da4e8708fbc
Tristan Pinaudeau <tristan@flexfactory.fr>
2019-04-07 16:49:56 +0200

    test : label can be set

--- tests/test-record.cpp
+++ tests/test-record.cpp
@@ -6,12 +6,17 @@
 
 #include "../src/Record.h"
 
-TEST_CASE("Records is created with empty label", "[record]") {
+TEST_CASE("Records usage.", "[record]") {
     Record record(3, 1, 0, 0);
     REQUIRE(record.get_sides() == 3);
     REQUIRE(record.get_right_angles() == 1);
     REQUIRE(record.get_parallels_sides() == 0);
     REQUIRE(record.get_same_length_sides() == 0);
     REQUIRE(record.get_label() == Label());
+
+    SECTION("Label can be set later.") {
+        record.set_label(SQUARE);
+        REQUIRE(record.get_label() == Label::SQUARE);
+    }
 }
 
