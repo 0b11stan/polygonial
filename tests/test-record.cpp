1c9eafd985c05319e51ec64fd4c1bc5cc8cb7666
Tristan Pinaudeau <tristan@flexfactory.fr>
2019-04-07 16:45:30 +0200

    test : record

--- tests/test-record.cpp
+++ tests/test-record.cpp
@@ -6,8 +6,12 @@
 
 #include "../src/Record.h"
 
-TEST_CASE("Records can be created", "[record]") {
+TEST_CASE("Records is created with empty label", "[record]") {
     Record record(3, 1, 0, 0);
     REQUIRE(record.get_sides() == 3);
+    REQUIRE(record.get_right_angles() == 1);
+    REQUIRE(record.get_parallels_sides() == 0);
+    REQUIRE(record.get_same_length_sides() == 0);
+    REQUIRE(record.get_label() == Label());
 }
 
