967d3aca0cde23e5cf2713855ec39fbb4bd4db34
Tristan Pinaudeau <tristan@flexfactory.fr>
2019-04-09 20:24:56 +0200

    refactor code

--- src/RulesEngine.cpp
+++ src/RulesEngine.cpp
@@ -2,15 +2,34 @@
 // Created by tristan on 04/04/19.
 //
 
+#include <backtrace.h>
 #include "RulesEngine.h"
 
 void RulesEngine::fill(Record &record) {
-    if (record.get_sides() == 3) record.set_label(TRIANGLE);
-    else if (record.get_sides() == 4) record.set_label(TETRAGON);
-    else if (record.get_sides() == 5) record.set_label(PENTAGON);
-    else if (record.get_sides() == 6) record.set_label(HEXAGON);
-    else if (record.get_sides() == 7) record.set_label(HEPTAGON);
-    else if (record.get_sides() == 8) record.set_label(OCTAOGON);
-    else if (record.get_sides() == 9) record.set_label(NONAGON);
-    else if (record.get_sides() == 10) record.set_label(DECAGON);
+    switch (record.get_sides()) {
+        case 3:
+            record.set_label(TRIANGLE);
+            break;
+        case 4:
+            record.set_label(TETRAGON);
+            break;
+        case 5:
+            record.set_label(PENTAGON);
+            break;
+        case 6:
+            record.set_label(HEXAGON);
+            break;
+        case 7:
+            record.set_label(HEPTAGON);
+            break;
+        case 8:
+            record.set_label(OCTAOGON);
+            break;
+        case 9:
+            record.set_label(NONAGON);
+            break;
+        case 10:
+            record.set_label(DECAGON);
+            break;
+    }
 }
