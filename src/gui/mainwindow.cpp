abd979f9713862803c3def49c684a926316d5268
TristanPinaudeau <tristan@flexfactory.fr>
2019-04-18 16:01:22 +0200

    feat: fill first combobox

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "iostream"
+#include <string>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int x=3; x <= 10; x++) {
+        ui->cb_sides->addItem(QString(std::to_string(x).c_str()));
+    }
 }
 
 MainWindow::~MainWindow()
